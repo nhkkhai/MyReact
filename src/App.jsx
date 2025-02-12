e8d8f3aefb16962f768137b3e44ea9f5a4470a83
Ninh Hoang Khai <nhkkhaii@gmail.com>
2025-02-12 09:40:36 +0700

    convert Component to file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-const MyComponent = () => {
-  return (
-    <div>Ninh Hoang Khai</div>
-  );
-}
+import MyComponent from './components/learn/MyComponent'
+import SecondComponent from './components/learn/SecondComponent'
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -23,7 +19,8 @@ const App = () => {
         </a>
       </div>
       <h1>Hello World</h1>
-      <MyComponent></MyComponent>
+      <MyComponent />
+      <SecondComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+const MyComponent = () => {
+    return (
+        <div>Ninh Hoang Khai 1231 123</div>
+    );
+}
+
+export default MyComponent
